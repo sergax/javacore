652c123aa6c0aa2f754e2152fb04433dc93ff8a1
sergax <serey.aksenchenko@gmail.com>
2021-12-01 00:01:09 +0200

    updating master

--- Test/tasks/TaskTesting.java
+++ Test/tasks/TaskTesting.java
@@ -1,1 +1,23 @@
+package tasks;
 
+import com.sergax.javacore.tasks.Task1;
+import com.sergax.javacore.tasks.Task2;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+public class TaskTesting {
+    @Test
+    public void TestingTask1() throws Exception {
+        Task1 task1 = new Task1();
+        System.out.println(Arrays.toString(task1.indexReturn(new int[]{5, 6, 4, 8}, 10)));
+        System.out.println(Arrays.toString(task1.indexReturnByMap(new int[]{5, 6, 4, 8,}, 10)));
+    }
+
+    @Test
+    public void TestingTask2() {
+        Task2 task2 = new Task2();
+        System.out.println(task2.duplicatesInArray(new int[]{4, 6, 5, 6}));
+        System.out.println(task2.duplicatesInArrayByList(new int[]{1}));
+    }
+}
--- src/main/java/com/sergax/javacore/tasks/Task1.java
+++ src/main/java/com/sergax/javacore/tasks/Task1.java
@@ -1,5 +1,9 @@
 package com.sergax.javacore.tasks;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 /*Задание 1
 
         Дан массив целых чисел. Необходимо реализовать метод, который возвращает индексы массива,
@@ -14,15 +18,34 @@ package com.sergax.javacore.tasks;
         Т.е. индексы 1 и 2 (значение 8 и 15) в сумме дают искомое число 23
 */
 public class Task1 {
-    public void indexReturn(int[] array, int number) {
+    public int[] indexReturn(int[] array, int number) throws Exception {
+        // O(n^2)
         for (int i = 0; i < array.length; i++) {
             for (int j = i + 1; j < array.length; j++) {
-                if (array[i] == number && array[j] == number) {
-                    System.out.println("result = [" + array[i] + "," + array[j] + "]");
-                } else {
-                    System.out.println("Can't find this number");
+                if (array[i] + array[j] == number) {
+                    return new int[]{i, j};
                 }
             }
         }
+        throw new Exception("Can't find this number");
+    }
+
+    public int[] indexReturnByMap(int[] array, int number) throws Exception {
+        // O(1)
+        Map<Integer, Integer> newMap = new HashMap<>();
+
+        for (int i = 0; i < array.length; i++) {
+            newMap.put(array[i], i);
+        }
+        for (int i = 0; i < array.length; i++) {
+            int newNumber = number - array[i];
+
+            if (newMap.containsKey(newNumber) && newMap.get(newNumber) != i) {
+                return new int[]{i, newMap.get(newNumber)};
+            }
+        }
+        throw new Exception("Can't find this number");
     }
 }
+
+
