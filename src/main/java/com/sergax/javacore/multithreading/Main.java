75368d6b7da37466602219004da9ee76fa306101
sergax <serey.aksenchenko@gmail.com>
2022-01-04 23:34:43 +0200

    updating master

--- src/main/java/com/sergax/javacore/multithreading/Main.java
+++ src/main/java/com/sergax/javacore/multithreading/Main.java
@@ -1,5 +1,21 @@
 package com.sergax.javacore.multithreading;
+/*
+Дан класс:
+public class Foo {
+  public void first(Runnable r) { print("first"); }
+  public void second(Runnable r) { print("second"); }
+  public void third(Runnable r) { print("third"); }
+}
 
+Один и тот же экземпляр данного класса будет вызван 3мя разными потоками.
+Поток А - будет вызывать метод first().
+Поток B - second().
+Поток С - third().
+Необходимо реализовать механизм и изменить программу таким образом, что методы класса Foo будут вызваны в правильном порядке.
+Пример:
+Вывод: "firstsecondthird"
+Мы не знаем, в каком порядке будут вызваны методы, но должны гарантировать порядок.
+ */
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         Foo foo = new Foo();
